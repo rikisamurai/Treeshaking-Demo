d191e747c902a680c1a09e27af03fbe74415744c
riki <shanyulong.riki@bytedance.com>
2023-08-03 11:33:49 +0800

    import core-js

---
+++ src/const.js
@@ -0,0 +1,31 @@
+/*＃NO_SIDE EFFECTS*/
+const addName/*#__NO_SIDE_EFFECTS__*/ = i => i + 'name';
+
+const name = /*#__PURE__*/addName('aakncaskjdnkjs1');
+export const Protocol = location.host.includes('boe') ? 'http' : 'https';
+export const Protocol1 = location.host.includes('boe') ?? 'https';
+console.log(function bar(price) {
+  return price || 0;
+})
+console.log(function bar(price) {
+  return price ?? 0;
+})
+
+export const URL = {
+  home: `${Protocol}https://home`,
+  blog: 'https://home/blog',
+  settings: 'https://home/settings',
+  profile: 'https://home/profile',
+}
+
+export const kiki = /*#__PURE__*/addName('kiki');
+export const riki = addName('riki');
+export const mugen = addName('mugen');
+
+export function foo(price) {
+  return price ?? 0;
+}
+
+export function bar(price) {
+  return price || 0;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,49 @@
 const path = require('path');
+const {WebDoctorWebpackPlugin} = require("@web-doctor/webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                targets: [
+                  '> 0.01%',
+                  'not dead',
+                  'not op_mini all'
+                ],
+                modules: false,
+                bugfixes: true,
+                shippedProposals: false,
+                useBuiltIns: 'usage',
+                exclude: [
+                  'transform-typeof-symbol'
+                ],
+                corejs: {
+                  version: '3.30',
+                  proposals: true
+                }
+              }]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new WebDoctorWebpackPlugin({
+      disableClientServer: false,
+    }),
+  ],
 };
